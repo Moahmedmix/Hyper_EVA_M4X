7deb7964be8592be8188f02098c0bfaf9867d734
𝐌𝐈𝐓𝐓𝐎 <153554481+Moahmedmix@users.noreply.github.com>
2026-07-02 00:37:14 +0300

    Update Hyper_EVADE.lua

--- Hyper_EVADE.lua
+++ Hyper_EVADE.lua
@@ -1,1 +1,11 @@
+--[[
+    ███╗   ██╗███████╗ ██████╗ 
+    ████╗  ██║██╔════╝██╔═══██╗
+    ██╔██╗ ██║█████╗  ██║   ██║
+    ██║╚██╗██║██╔══╝  ██║   ██║
+    ██║ ╚████║███████╗╚██████╔╝
+    ╚═╝  ╚═══╝╚══════╝ ╚═════╝ 
+        H Y P E R   E V A D E
+--]]
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Moahmedmix/Hyper_EVA_M4X/refs/heads/main/Mix/Game/EVADE/Neo_Hyper_EVADE.lua"))()
